406b3d942e9eba37153e292894f605e42f3cc8b3
TomKalapurackal <tom.kalapurackal@credr.com>
2017-04-26 00:10:05 +0530

    Setting Changes

--- axis_site/settings.py
+++ axis_site/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Third party
-    'djcelery',
     # 'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
